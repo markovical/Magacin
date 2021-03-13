bfe03b709a6ef6b205f1d62fc8ee445ee3842042
markovical <aca997@gmail.com>
2021-03-13 16:56:18 +0100

    Napravljena klasa Magacin koja treba implementira metode interfejsa ali je trenutno nefunkcionalna

---
+++ src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import java.util.LinkedList;
+
+import magacin.interfejs.MagacinInterface;
+
+public class Magacin implements MagacinInterface {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+
+	}
+
+	@Override
+	public LinkedList<Artikal> vratiArtikal(int sifra) {
+		
+		return null;
+	}
+
+}
