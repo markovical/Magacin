664328232ac056b1d550c2c869314e0dbfb41811
markovical <aca997@gmail.com>
2021-03-13 17:43:30 +0100

    Dodata je logicka kontrola u klasu Magacin na drugi nacin

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -5,12 +5,23 @@ import java.util.LinkedList;
 import magacin.interfejs.MagacinInterface;
 
 public class Magacin implements MagacinInterface {
-
+		
+	private LinkedList<Artikal> artikli = new LinkedList<Artikal>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-
+		
+		if(artikal == null)
+			return;
+		
+		for (Artikal a : artikli) {
+			if(a.getSifra() == artikal.getSifra()) {
+				a.setKolicina(a.getKolicina()+1);
+				return;
+			}
+		}
+		artikli.add(artikal);
 	}
-
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
 
