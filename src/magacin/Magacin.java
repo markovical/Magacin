ba736d65fc9daab2ce1454af03e06e4872e26111
markovical <aca997@gmail.com>
2021-03-13 17:26:22 +0100

    Implementirane metode interfejsa MagacinInterface u okviru klase Magacin

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -6,20 +6,53 @@ import magacin.interfejs.MagacinInterface;
 
 public class Magacin implements MagacinInterface {
 
+	private LinkedList<Artikal> artikli = new LinkedList<Artikal>();
+
 	@Override
 	public void dodajArtikal(Artikal artikal) {
+		if(artikal == null)
+			throw new NullPointerException("Artikal ne sme biti null!");
 
+			for (Artikal a : artikli) {
+				if(a.getSifra() == a.getSifra()) {
+					
+					a.setKolicina(a.getKolicina()+1); 
+					System.out.println("Poveæana kolièina artikla");
+					return;
+				}
 	}
-
+			artikli.add(artikal);
+	}
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
+		
+			if(artikal == null)
+				throw new NullPointerException("Artikal ne sme biti null!");
+			
+			 if(!artikli.contains(artikal)) 
+				 throw new RuntimeException("Artikal nije u magacinu!");
 
+			 for (Artikal a : artikli) {
+				 
+					if(a.getSifra() == artikal.getSifra() && a.getKolicina() > 0) {
+						a.setKolicina(a.getKolicina()-1); 
+						System.out.println("Smanjena kolièina artikla");
+						return;
+					}
+				}
 	}
 
 	@Override
 	public LinkedList<Artikal> vratiArtikal(int sifra) {
+		LinkedList<Artikal> rezultat = new LinkedList<Artikal>();
+		if(sifra <= 0)
+			throw new RuntimeException("Sifra artikla ne sme biti negativna ili 0");
 		
-		return null;
+		for (Artikal a : artikli) {
+			if(a.getSifra()  == sifra)
+				rezultat.add(a);
+				return rezultat;
 	}
-
+		return null;
+}
 }
